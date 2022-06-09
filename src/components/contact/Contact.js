0d6a74b68d12e685609b5d117c3adb97d2b34a97
jrtanmoy <roy28301@gmail.com>
2022-06-09 19:41:03 +0600

    implement emailjs for receive email from my portfolio

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,20 @@
-import React from 'react';
 import './Contact.css';
 import {MdOutlineEmail} from 'react-icons/md';
 import {RiMessengerLine} from 'react-icons/ri';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_gzhqacw', 'template_2wh98hf', form.current, '_TA52A-ysbO-rX57D')
+          
+        e.target.reset()
+      };
+
     return (
         <section id='contact'>
            <h5>Get In Touch</h5>
@@ -26,7 +37,7 @@ const Contact = () => {
                </div>
                {/* -----End Of Contact Option */}
 
-               <form action="">
+               <form ref={form} onSubmit={sendEmail}>
                    <input type="text" name='name' placeholder='Your Full Name' required />
                    <input type="email" name='email' placeholder='Your Email' required />
                    <textarea name="message" rows="10" placeholder='Your Message' required></textarea>
