d768df0f52bdd737bb1fb67cf8c0a5814cb12fdc
jrtanmoy <roy28301@gmail.com>
2022-06-09 07:03:34 +0600

    Basic setup

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            this is header.
+        </div>
+    );
+};
+
+export default Header;
