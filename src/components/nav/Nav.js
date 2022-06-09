a47955032db91d622519eeae9b950807fb7bff9f
jrtanmoy <roy28301@gmail.com>
2022-06-09 21:27:56 +0600

    update some css on projects section

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -6,9 +6,9 @@ const Nav = () => {
     return (
         <nav>
             <a href="#"  onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}>Home</a>
-            <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}>About Me</a>
-            <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}>Skills</a>
-            <a href="#projects" onClick={() => setActiveNav('#projects')} className={activeNav === '#projects' ? 'active' : ''}>Projects</a>
+            <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}>About</a>
+            <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}>Skill</a>
+            <a href="#projects" onClick={() => setActiveNav('#projects')} className={activeNav === '#projects' ? 'active' : ''}>Project</a>
             <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}>Contact</a>
         </nav>
     );
