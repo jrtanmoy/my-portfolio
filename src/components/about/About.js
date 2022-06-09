ec026db047040d64efc28913b02a1fcb0b94b9a5
jrtanmoy <roy28301@gmail.com>
2022-06-09 12:32:44 +0600

    about section done

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './About.css'
+import AboutMe from '../../assets/about-me2.jpg'
 
 const About = () => {
     return (
         <section id='about'>
-            About
+            <h5>Get To Know</h5>
+            <h2>About Me</h2>
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about__me-image">
+                        <img src={AboutMe} alt="about me" />
+                    </div>
+                </div>
+                <div className='about__content'>
+                    <p>
+                        Hi there, I am Joy Roy. I am a Junior MERN Stack Developer and having a vast interest to work with technology and eagerness to know more made my way of learning smoother, hopefully it will continuously improve. I always want to learn by doing and get enough experience to be a senior Web Developer.
+                    </p>
+                    <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+                </div>
+            </div>
         </section>
     );
 };
