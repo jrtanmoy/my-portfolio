d30c27bb50ddd69810474e9a3ec2a62e8de902ff
jrtanmoy <roy28301@gmail.com>
2022-06-10 21:57:29 +0600

    some changes in ui

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -44,7 +44,7 @@ const Contact = () => {
                    <input type="text" name='name' placeholder='Your Full Name' required />
                    <input type="email" name='email' placeholder='Your Email' required />
                    <textarea name="message" rows="10" placeholder='Your Message' required></textarea>
-                   <button type='submit' className='btn btn-primary'>Send Message</button>
+                   <button type='submit' className='btn btn-primary'>Send Email</button>
                </form>
            </div>
         </section>
